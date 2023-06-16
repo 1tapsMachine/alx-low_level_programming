8f0952570a320bf1bbf0eb8335151d3e584134af
aziz sami <samiztsouhailsami@gmail.com>
2023-06-16 17:36:57 +0100

    combinaison++ ++

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,9 +11,9 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 9; j++)
 {
-for (k = j; k < 10; k++)
+for (k = i; k < 10; k++)
 {
-for (l = k; l < 10; l++)
+for (l = j+1; l < 10; l++)
 {
 putchar(i + '0');
 putchar(j + '0');
