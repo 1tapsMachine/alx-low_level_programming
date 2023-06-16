d8a743f99dadbf95ada5c9c3060c333f352f4a17
aziz sami <samiztsouhailsami@gmail.com>
2023-06-16 16:03:58 +0100

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,19 +6,20 @@
 */
 int main(void)
 {
-int numbers[]={0,1,2,3,4,5,6,7,8,9};
+int numbers[]={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 char letters[]={'a','b','c','d','e','f'};
 int i = 0;
 while (i < 10)
 {
-putchar(numbers[i]+'0');
+putchar(numbers[i] + '0');
 i++;
 }
-i = 0; 
-while(i < 6)
+i = 0;
+while (i < 6)
 {
 putchar(letters[i]);
 i++;
 }
+putchar('\n');
 return (0);
 }
