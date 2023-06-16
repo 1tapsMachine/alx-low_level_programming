208eb885f702f3ab0d8b766ede81b21744cbea63
aziz sami <samiztsouhailsami@gmail.com>
2023-06-16 17:39:15 +0100

    combinaison++ ++

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,11 +9,11 @@ int main(void)
 int i, j, k, l;
 for (i = 0; i < 10; i++)
 {
-for (j = 0; j < 9; j++)
+for (j = 0; j < 10; j++)
 {
 for (k = i; k < 10; k++)
 {
-for (l = j+1; l < 10; l++)
+for (l = j + 1; l < 10; l++)
 {
 putchar(i + '0');
 putchar(j + '0');
