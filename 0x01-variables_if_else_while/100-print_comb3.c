f09465c1438b6887d8426c2b40b9c3faf1621683
aziz sami <samiztsouhailsami@gmail.com>
2023-06-16 17:06:31 +0100

    combinaison++

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+for (int i = 0; i < 9; i++)
+{
+for (int j = i; i < 10; j++)
+{
+if (i != j)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i != 8 || j != 9)
+{
+putchar(' ');
+putchar(',');
+}
+}
+}
+}
+return (0);
+}
