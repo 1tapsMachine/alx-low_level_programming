88937caa5023c5c421f55f465b082672cb6eb574
aziz sami <samiztsouhailsami@gmail.com>
2023-06-16 16:00:36 +0100

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - printing all base 16 numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int numbers[]={0,1,2,3,4,5,6,7,8,9};
+char letters[]={'a','b','c','d','e','f'};
+int i = 0;
+while (i < (sizeof(numbers) / sizeof(int)))
+{
+putchar(numbers[i]+'0');
+i++;
+}
+i = 0; 
+while(i < (sizeof(letters) / sizeof(char)))
+{
+putchar(letters[i]);
+i++;
+}
+return (0);
+}
