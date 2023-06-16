bcae727c3c7a1ec832c247bf2abadb76e926ce4f
aziz sami <samiztsouhailsami@gmail.com>
2023-06-16 14:19:06 +0100

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,14 +16,14 @@ if (n < 0)
 {
 n = abs(n);
 lastDigit = -(n % 10);
+printf("Last digit of -%d is %d and is greater than 5\n", n, lastDigit);
 }
 else if (n > 0)
 {
 lastDigit = n % 10;
-}
 if (lastDigit > 5)
 {
-printf("Last digit of -%d is %d and is greater than 5\n", n, lastDigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else if (lastDigit < 6 && lastDigit != 0)
 {
@@ -33,6 +33,6 @@ else
 {
 printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
-
+}
 return (0);
 }
