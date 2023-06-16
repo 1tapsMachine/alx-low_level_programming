79367c3bf2b181cbf40c7009f28ad64d69f96766
aziz sami <samiztsouhailsami@gmail.com>
2023-06-16 14:39:05 +0100

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,13 @@
 */
 int main(void)
 {
-char alphabet[26] = "abcdefghijklmnopqrstuvwxyz\n";
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 int i = 0;
 while (i < 26)
 {
 putchar(alphabet[i]);
 i++;
 }
+putchar('\n');
 return (0);
 }
