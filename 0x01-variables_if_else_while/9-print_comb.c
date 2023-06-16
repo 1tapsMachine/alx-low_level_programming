ffa5d61dd82b1432b06ac7298c3945af7adc8ab7
aziz sami <samiztsouhailsami@gmail.com>
2023-06-16 16:50:46 +0100

    combinaison

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,11 +6,11 @@
 */
 int main(void)
 {
-int digit=0;
-while (digit< 10)
+int digit = 0;
+while (digit < 10)
 {
 putchar(digit + '0');
-if(digit < 9)
+if (digit < 9)
 {
 putchar(',');
 putchar(' ');
