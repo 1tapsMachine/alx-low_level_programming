d75374afd85e6f4827f5aa5a42e4ce0886f0833e
Sami <samiztsouhailsami@gmail.com>
2023-06-21 00:28:34 +0100

    fabioTrick

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - prints fibonacci sequence up to 50 numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, count;
+unsigned long last, this, next;
+last = 1;
+this = 2;
+count = 97;
+for (i = 0; i < count; i++)
+{
+printf("%lu, ", last);
+next = last + this;
+last = this;
+this = next;
+}
+printf("%lu", last);
+printf("\n");
+return (0);
+}
