63ceba4c1d1a0c3a58bb923b74cbd0b306f1d4f6
Sami <samiztsouhailsami@gmail.com>
2023-06-20 17:35:18 +0100

    hotfixedIslower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-* main - Entry point
+* _islower - checks for lowercase character
 *
-* Return: Always 0 (Success)
+* Return: 1 if c is lowercase, 0 otherwise
 */
 int _islower(int c)
 {
 char alpha = 'a';
-for(alpha= 'a';alpha<= 'z';alpha++)
+for (alpha = 'a' ; alpha <= 'z' ; alpha++)
 {
-if(alpha==c)
+if (alpha == c)
 {
 return (1);
 }
