e0016930586182783c70dddb508a50c0559106f8
Sami <samiztsouhailsami@gmail.com>
2023-06-20 19:03:13 +0100

    lastD

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,17 @@
  */
 int print_last_digit(int n)
 {
+int last_digit;
 if (n < 0)
 {
-return (-n % 10);
+last_digit = -1 * (n % 10);
+_putchar(last_digit + '0');
+return (last_digit);
 }
 else if (n > 0)
 {
-return (n % 10);
-}
-else
-{
-return (0);
+last_digit = n % 10;
+_putchar(last_digit + '0');
+return (last_digit);
 }
 }
