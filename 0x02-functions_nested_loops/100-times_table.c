3fcf06f3a81f3727642bd91cb62ff9b3d5399a66
Sami <samiztsouhailsami@gmail.com>
2023-06-20 21:47:53 +0100

    fixedTimeTable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,31 +8,40 @@
 */
 void print_times_table(int n)
 {
-int i, j, k;
+int i, j, prod;
 if (n >= 0 && n <= 15)
 {
-for (i = 0; i < n; i++)
+for (i = 0; i <= n; i++)
 {
-for (j = 0; j < n; j++)
+for (j = 0; j <= n; j++)
 {
-k = i * j;
+prod = i * j;
 if (j == 0)
 {
-printf("%d", k);
+printf("%d", prod);
 }
-else if (k < 10)
+else if (prod < 10)
 {
-printf(",   %d", k);
+printf(",   %d", prod);
 }
-else if (k >= 10 && k < 100)
+else if (prod >= 10 && prod < 100)
 {
-printf(",  %d", k);
+printf(",  %d", prod);
 }
-else if (k >= 100)
+else if (prod >= 100)
 {
-printf(", %d", k);
+printf(", %d", prod);
+}
+}
+printf("\n");
 }
 }
 }
-}
+
+
+
+
+int main(void)
+{
+print_times_table(3);
 }
