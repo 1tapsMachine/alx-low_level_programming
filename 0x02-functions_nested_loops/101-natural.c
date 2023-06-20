875f1d343abd55658c192cba3cbe3270d1fb25e7
Sami <samiztsouhailsami@gmail.com>
2023-06-20 23:36:47 +0100

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* fibonacci - prints the first 50 Fibonacci 
+* fibonacci - prints the first 50 Fibonacci
 * numbers, starting with 1 and 2
 *
 * Return: void
@@ -8,11 +8,11 @@
 int main(void)
 {
 int i, sum;
-for(i = 0; i < 1024; i++)
+for (i = 0; i < 1024; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
 {
-sum+=i;
+sum += i;
 }
 }
 printf("%d\n", sum);
