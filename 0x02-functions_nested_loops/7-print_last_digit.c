ab11b91010bf3db801f4ad745a6a60e8196d62a0
Sami <samiztsouhailsami@gmail.com>
2023-06-20 19:04:08 +0100

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,12 +14,11 @@ if (n < 0)
 {
 last_digit = -1 * (n % 10);
 _putchar(last_digit + '0');
-return (last_digit);
 }
 else if (n > 0)
 {
 last_digit = n % 10;
 _putchar(last_digit + '0');
-return (last_digit);
 }
+return (last_digit);
 }
