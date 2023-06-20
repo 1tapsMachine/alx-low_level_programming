b43a7eefbea27c00b9334731c7783e05251191af
Sami <samiztsouhailsami@gmail.com>
2023-06-20 17:20:40 +0100

    fixedBetty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
-* print_alphabet_x10 - prtints alphabet ten times in lowercase followed by a new line
+* print_alphabet_x10 - prtints alphabet ten times
+* in lowercase followed by a new line
 *
 * Return: Always 0 (Success)
 */
 void print_alphabet_x10(void)
 {
 int i = 0;
-for(i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
 char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 int i = 0;
