9cc152134796e7ad8d6022bd9db50bb2d97536c1
Sami <samiztsouhailsami@gmail.com>
2023-06-22 17:18:05 +0100

    printNUmber

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* print_number - prints an integer
+*
+* @n : int
+*
+* Return: Always 0 (Success)
+*/
+void print_number(int n)
+{
+	if (n < 0)
+	{
+
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 > 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
+}
