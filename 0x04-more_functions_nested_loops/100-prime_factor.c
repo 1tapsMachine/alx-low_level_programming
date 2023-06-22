c0b37e4a4d227820d7848179905d02f477632d8c
Sami <samiztsouhailsami@gmail.com>
2023-06-22 16:39:51 +0100

    done

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,22 +6,22 @@
 */
 int main(void)
 {
-    long int number = 612852475143; 
-    int factor = 2, largest_factor;
-    while (number != 0)
-    {
-        if (number % factor != 0)
-            factor += 1;
-        else
-        {
-            largest_factor = number;
-            number = number / factor;
-            if (number == 1)
-            {
-                printf("%d\n", largest_factor);
-                break;
-            }
-        }
-    }
-    return (0);
+	long int number = 612852475143;
+	int factor = 2, largest_factor;
+	while (number != 0)
+	{
+		if (number % factor != 0)
+			factor += 1;
+		else
+		{
+			largest_factor = number;
+			number = number / factor;
+			if (number == 1)
+			{
+				printf("%d\n", largest_factor);
+				break;
+			}
+		}
+	}
+	return (0);
 }
