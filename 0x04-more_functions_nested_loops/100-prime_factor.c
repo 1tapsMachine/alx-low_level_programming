672740e217c557c25d2573cfe2aac2178ce63e5b
Sami <samiztsouhailsami@gmail.com>
2023-06-22 16:31:05 +0100

    prime_factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+    long int number = 1231952; 
+    int factor = 2, largest_factor;
+    while (number != 0)
+    {
+        if (number % factor != 0)
+            factor += 1;
+        else
+        {
+            largest_factor = number;
+            number = number / factor;
+            if (number == 1)
+            {
+                printf("%d\n", largest_factor);
+                break;
+            }
+        }
+    }
+}
