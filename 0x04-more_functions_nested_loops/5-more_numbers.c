82f234472c33ad11a966a1745768181a4f2e9c4f
Sami <samiztsouhailsami@gmail.com>
2023-06-22 14:44:14 +0100

    moreNumbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
  * more_numbers - prints 10 times the numbers
  * from 0 to 14, followed by a new line
  *
- * Return: Always 0 (Success)
+ * Return: VOID
  */
 
 void more_numbers(void)
@@ -14,13 +14,13 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-            if (j >= 10)
-            {
-                _putchar('1');
-            }
-                _putchar((j % 10) + '0');
-                continue;
+			if (j >= 10)
+			{
+				_putchar('1');
+			}
+				_putchar((j % 10) + '0');
+				continue;
 		}
-        _putchar('\n');
+		_putchar('\n');
 	}
 }
