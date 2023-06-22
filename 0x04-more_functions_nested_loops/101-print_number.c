60b76102b9f9147c86958a16f5579caf62d69afe
Sami <samiztsouhailsami@gmail.com>
2023-06-22 17:28:42 +0100

    done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,9 +14,13 @@ void print_number(int n)
 		_putchar(45);
 		n = -n;
 	}
-	if (n / 10 > 0)
+    else
+    {
+        n = n;
+    }
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar((n % 10)+ '0');
 }
