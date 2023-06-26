4a888ff7d53eb982b1a80b3729e6fc1e388810cc
Sami <samiztsouhailsami@gmail.com>
2023-06-26 14:08:26 +0100

    fixedAtoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,16 +4,19 @@
 *
 * @s: string to convert
 *
-* Return: 
+* Return: the converted string
 */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, res = 0, found = 0;
+	int i = 0, sign = 1, found = 0;
+	unsigned int res = 0;
 
 	while (*(s + i))
 	{
 		if (*(s + i) == '-')
+		{
 			sign *= -1;
+		}
 		if (*(s + i) >= '0' && *(s + i) <= '9')
 		{
 			res *= 10;
