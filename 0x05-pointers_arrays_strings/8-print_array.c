da51db338c6566191ccd38af5dcd5efe0ad35f10
Sami <samiztsouhailsami@gmail.com>
2023-06-26 12:13:28 +0100

    fixedBetty

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,6 +2,9 @@
 /**
 * print_array - prints n elements of an array of integers
 *
+* @a: array of integers
+* @n: number of elements to be printed
+*
 * Return: void
 */
 void print_array(int *a, int n)
@@ -10,7 +13,7 @@ void print_array(int *a, int n)
 
 	while (i < n)
 	{
-		if (i == n -1 )
+		if (i == n - 1)
 		{
 			printf("%d", *(a + i));
 		}
