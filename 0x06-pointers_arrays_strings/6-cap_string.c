614675aed9035713b8cd4cd32d04a3b39552fe24
Sami <samiztsouhailsami@gmail.com>
2023-06-29 20:44:19 +0100

    fixedCap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,31 +9,45 @@
 */
 char *cap_string(char *str)
 {
-	int i = 0;
+	char *separators = " \t\n,;.!?\"(){}";
+	int i = 0, j = 0, flag = 0;
 
 	for (; str[i] != '\0'; i++)
 	{
-		if (i == 0)
+		if (flag)
 		{
+
+			if ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'A' && str[i] <= 'Z'))
+				flag = 0;
 			if (str[i] >= 'a' && str[i] <= 'z')
+			{
 				str[i] -= 32;
+				flag = 0;
+			}
 		}
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		else
 		{
-			++i;
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] -= 32;
-		}
-		if (
-		(str[i] == '.' || str[i] == ';' || str[i] == ',' || str[i] == '!' ||
-		str[i] == '?' || str[i] == ',' || str[i] == '"' || str[i] == '(' ||
-		str[i] == ')' || str[i] == '{' || str[i] == '}')
-		&& (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
-		{
-			++i;
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] -= 32;
+			if (str[0] >= 'a' && str[0] <= 'z')
+			{
+				flag = 1;
+			}
+			for (j = 0; separators[j] != '\0'; j++)
+			{
+				if (str[i] == separators[j])
+					flag = 1;
+			}
 		}
 	}
 	return (str);
+}
+
+int main(void)
+{
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char *ptr;
+
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
 }
