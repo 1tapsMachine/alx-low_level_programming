0b32a0f33002d51ad742074eac1311a9fff0d573
Sami <samiztsouhailsami@gmail.com>
2023-06-29 19:59:02 +0100

    capLetter

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,35 @@
 #include "main.h"
+#include <string.h>
 /**
 * cap_string - capitalizes all words of a string
 *
 * @str: string to be capitalized
 *
-* Return: 
+* Return: the resulting string
 */
 char *cap_string(char *str)
 {
-	
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		{
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		if (str[i] == '.' && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+		{
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+	}
+	return (str);
 }
