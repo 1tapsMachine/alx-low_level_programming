8c22750c4516871c49a0e34e0c4021b4c766c044
Sami <samiztsouhailsami@gmail.com>
2023-07-05 14:59:20 +0100

    done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,32 +1,53 @@
 #include "main.h"
+
 /**
- * get_last_char - gets the last character of a string
+ * _strlen - calculates the length of a string
  *
- * @s: string to get last character of
+ * @s: string to calculate length of
  *
- * Return: last character of string
-*/
-int get_last_char(char *s)
+ * Return: length of the string
+ */
+int _strlen(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	return (1 + get_last_char(s + 1));
+    if (*s == '\0')
+        return 0;
+    return 1 + _strlen(s + 1);
 }
+
+/**
+ * is_palindrome_helper - recursive helper function for is_palindrome
+ *
+ * @s: string to check
+ * @start: starting index
+ * @end: ending index
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+    if (start >= end)
+        return 1;
+
+    if (s[start] != s[end])
+        return 0;
+
+    return is_palindrome_helper(s, start + 1, end - 1);
+}
+
 /**
  * is_palindrome - checks if a string is a palindrome
  *
  * @s: string to check
  *
- * Return: (1) if palindrome, (0) if not
-*/
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
 int is_palindrome(char *s)
 {
-	if (*s != *(s + get_last_char(s) - 1))
-	{
-		return (0);
-	}
-	else
-	{
-		return (1);
-	}
+    int len = _strlen(s);
+
+    if (len <= 1)
+        return 1;
+
+    return is_palindrome_helper(s, 0, len - 1);
 }
+
