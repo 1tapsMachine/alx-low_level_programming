16641378a2f0cea6ec8076c9d6f84a644f1f713a
Sami <samiztsouhailsami@gmail.com>
2023-07-05 15:56:15 +0100

    maybeDone

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,55 +1,31 @@
 #include "main.h"
 /**
- * _strlen_recursion - returns the length of a string
- * @s: string to be counted
- * Return: length of string
+ * wildcmp - compares two strings and returns 1
+ * if the strings can be considered identical,
+ * otherwise return 0.
+ * @s1: the normal string
+ * @s2: the special string containing "*"
+ *
+ * Return: 1 if identical, else 0
  */
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(++s));
-}
-/**
- * check - checks if string is identical
- * @s1: string to be checked
- * @s2: string to be checked
- * @i: index
- * @j: index
- * @len1: length of string
- * @len2: length of string
- * Return: 1 if identical, 0 if not
- */
-int check(char *s1, char *s2, int i, int j, int len1, int len2)
-{
-	if (j == len2)
-		return (1);
-	if (s2[j] == '*')
-		return (check(s1, s2, i, j + 1, len1, len2));
-	else if (s2[j] == s1[i])
-		return (check(s1, s2, i + 1, j + 1, len1, len2));
-	else if (s2[j] != s1[i])
-	{
-		if (s1[i] == '\0')
-			return (0);
-		else if (s2[j] == '*')
-		{
-			return (check(s1, s2, i + 1, j, len1, len2));
-		}
-	}
-	return (0);
-}
-/**
- * wildcmp - compares two strings
- * @s1: string to be compared
- * @s2: string to be compared
- * Return: 1 if strings are identical, 0 if not
-*/
 int wildcmp(char *s1, char *s2)
 {
-	int len1, len2;
+	if (*s2 == '\0' && *s1 == '\0')
+	{
+		return (1);
+	}
+	else if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	{
+		return (0);
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
 
-	len1 = _strlen_recursion(s1);
-	len2 = _strlen_recursion(s2);
-	return (check(s1, s2, 0, 0, len1, len2));
+	return (0);
 }
