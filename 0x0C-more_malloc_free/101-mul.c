41db5298468a6d95af15a83387a2c2c980211bd6
Sami <samiztsouhailsami@gmail.com>
2023-07-14 01:23:20 +0100

    WAL3ADAAAAAAAAAAAAAAAW

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,132 +1,99 @@
-#include "main.h"
-#include <unistd.h>
-int _atoi(char *s);
-int _isdigit(int c);
-int is_numeric(char *str);
-void putint(int n);
-unsigned long multiply(unsigned int *num1, unsigned int *num2);
+#include <stdio.h>
+#include <stdlib.h>
+unsigned long _atoi(char *s);
 /**
- * main - multiplies two positive numbers.
- * @argc: number of command line arguments.
- * @argv: arr of str that contains the arguments.
- * Return: 0.
+ * mul - a function that multiplies two integers.
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the multiplication
  */
-int main(int argc, char **argv)
+unsigned int mul(unsigned int a, unsigned int b)
 {
-	unsigned int *num1, *num2;
-	unsigned long mul;
+	return (a * b);
+}
+/**
+ * isDgt - a function that checks if a buffer has only digits.
+ * @buffer: buffer that will be tested.
+ * Return: 0 if a non-digit was found, 1 otherwise
+ */
+int isDgt(char *buffer)
+{
+	int add = 0;
 
-	if (argc != 3 || !is_numeric(argv[1]) || !is_numeric(argv[2]))
+	while (buffer[add] != '\0')
 	{
-		write(1, "Error\n", 7);
-		exit(98);
-	}
-	num1 = malloc(sizeof(unsigned int));
-	num2 = malloc(sizeof(unsigned int));
-	if (num1 == NULL || num2 == NULL)
+		if (!(buffer[add] >= '0' && buffer[add] <= '9'))
+			return (0);
 
+		add++;
+	}
+
+	return (add);
+}
+
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: count of arguments given to the programs
+ * @argv: arguments given to the program.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+
+	char *strBuf;
+	char *strBuf2;
+	int nbr1, nbr2;
+	unsigned int result,val1,val2;
+
+	if (argc < 3)
 	{
-		write(1, "Error\n", 7);
-		free(num1);
-		free(num2);
-		exit(98);
+		printf("Error\n");
+		return (98);
 	}
-	*num1 = _atoi(argv[1]);
-	*num2 = _atoi(argv[2]);
-	mul = multiply(num1, num2);
-	putint(mul);
-	putchar('\n');
 
-	free(num1);
-	free(num2);
+	strBuf = argv[1];
+	strBuf2 = argv[2];
+	nbr1 = isDgt(strBuf);
+	nbr2 = isDgt(strBuf2);
 
+	if ((nbr1 == 0) || (nbr2 == 0))
+	{
+		printf("Error\n");
+		return (98);
+	}
+	val1 = _atoi(strBuf);
+	val2 = _atoi(strBuf2);
+	result = mul(val1, val2);
+	printf("%d\n", result);
 	return (0);
 }
-
 /**
- * is_numeric - function to check if a string is composed only of digits
- * @str: ptr to string.
- * Return: 1 or 0.
- */
-int is_numeric(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (!_isdigit(str[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
-/**
- * _atoi - convert str to int
- * @s: the string
+ * _atoi - convert a string to an integer
  *
- * Return: the number in integer
+ * @s: string to convert
+ *
+ * Return: the converted string
  */
-
-int _atoi(char *s)
+unsigned long _atoi(char *s)
 {
-	unsigned int num = 0;
-	int i = 0;
-	int sign = 1;
+	int i = 0, sign = 1, found = 0;
+	unsigned int res = 0;
 
 	while (*(s + i))
 	{
 		if (*(s + i) == '-')
+		{
 			sign *= -1;
-		else if (*(s + i) >= '0' && *(s + i) <= '9')
-			num = (num * 10) + (*(s + i) - '0');
-		else if (num > 0)
+		}
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			res *= 10;
+			res += *(s + i) - '0';
+			found = 1;
+		}
+		else if (found)
 			break;
 		i++;
 	}
-	return (num * sign);
-}
-/**
- * _isdigit - return 1 if the character is a digit, and 0 otherwise.
- * @c: the number to be checked
- * Return: Always 1 for true and 0 for false.
- */
-int _isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
-/**
- * putint - print integer using putchar.
- * @n: integer.
- *
- */
-void putint(int n)
-{
-	if (n < 0)
-	{
-		putchar('-');
-		n = -n;
-	}
-	if (n > 9)
-	{
-		putint(n / 10);
-	}
-	putchar(n % 10 + '0');
-}
-/**
- *  multiply - function to multiply two numbers
- *@num1: ptr to num1.
- *@num2: ptr to num2.
- *Return: mul.
- */
-unsigned long multiply(unsigned int *num1, unsigned int *num2)
-{
-	return ((*num1) * (*num2));
+	return (res * sign);
 }
