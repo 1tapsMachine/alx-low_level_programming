20989caa33c444de4793df0be732e7d4f082e8fe
Sami <samiztsouhailsami@gmail.com>
2023-07-17 13:06:57 +0100

    hotfix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+  * struct dog - Structure.
+  * @name: Name.
+  * @age: Age.
+  * @owner: Owner.
+  */
 struct dog
 {
-	name char *;
-	age float;
-	owner char *;
+	char *name;
+	float age;
+	char *owner;
 };
