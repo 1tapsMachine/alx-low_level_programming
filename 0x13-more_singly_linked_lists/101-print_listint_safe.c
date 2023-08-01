5a0a0897a6e84bf00a79bf07ffffecce830f5ac4
Sami <samiztsouhailsami@gmail.com>
2023-08-01 14:27:55 +0100

    last commit

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t list.
+ * @h: pointer to the first element of the list.
+ *
+ * Return: the size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t size;
+	listint_t *current_node, *next_node;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	size = 0;
+	current_node = *h;
+
+	while (current_node != NULL)
+	{
+		size++;
+		next_node = current_node->next;
+		free(current_node);
+
+		if (current_node < next_node)
+			break;
+
+		current_node = next_node;
+	}
+
+	*h = NULL;
+
+	return (size);
+}
