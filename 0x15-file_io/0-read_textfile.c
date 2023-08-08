d27727951a8ff29fcf4c1397dff64b340c6cb541
Sami <samiztsouhailsami@gmail.com>
2023-08-08 01:28:19 +0100

    edit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,25 +11,34 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp;
-	ssize_t nrd, nwr;
+	int fd;
 	char *buf;
+	ssize_t r, w;
 
 	if (!filename)
 		return (0);
-	fp = open(filename, O_RDONLY);
-	if (fp == -1)
-		return (0);
-	buf = malloc(sizeof(char) * (letters));
+	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
-
-	nrd = read(fp, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-	close(fp);
-
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	r = read(fd, buf, letters);
+	if (r == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buf, r);
+	if (w == -1 || w != r)
+	{
+		free(buf);
+		return (0);
+	}
 	free(buf);
-
-	return (nwr);
+	close(fd);
+	return (w);
 }
